3be19e10cfba30c48245acb0a2793d858fc7fd37
Reidy07 <reidybrown1234@gmail.com>
2024-08-18 11:29:27 -0400

    Agregar archivos de proyecto.

---
+++ Practica DI/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Practica_DI
+{
+         public class CustomerRepository
+        {
+            public void SaveCustomer(string name)
+            {
+            
+            }
+        }
+        public class CustomerService
+        {
+            private CustomerRepository _repository;
+
+            public CustomerService()
+            {
+                _repository = new CustomerRepository();
+            }
+
+            public void AddCustomer(string name)
+            {
+                _repository.SaveCustomer(name);
+            }
+        }    
+}
